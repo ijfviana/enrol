126186ab808ade56ccd1169ba7798e18217becdb
juanmacalvo <48252840+juanmacalvo@users.noreply.github.com>
2019-10-04 11:24:42 +0200

    checkbox problem solved

--- course_mapping_form.php
+++ course_mapping_form.php
@@ -87,7 +87,7 @@ class course_mapping_editadvanced_form extends moodleform {
 
 
 
-        $mform->addElement('checkbox', 'blocked', get_string('blocked', 'enrol_saml'), null);
+        $mform->addElement('advcheckbox', 'blocked','' ,get_string('blocked', 'enrol_saml'));
         $mform->addHelpButton('blocked', 'blocked', 'enrol_saml');
         //$mform->setDefault('status', $plugin->get_config('status'));
 
